16972955bb4127fd8fe9ce0f2a3cba6404a9a9f0
Oleksandra Pryhoda <sasaprigoda6@gmail.com>
2026-04-18 18:47:43 +0100

    added base to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: '/sincere/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
